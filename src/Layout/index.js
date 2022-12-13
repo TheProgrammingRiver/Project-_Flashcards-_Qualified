450dea52dc7a3c3242e717ee5a208cf0a01a2990
TheProgrammingRiver <114197158+TheProgrammingRiver@users.noreply.github.com>
2022-12-13 12:25:16 -0500

    mm

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import Header from "./Header";
-import DeckHome from "../Decks/DeckHome";
+import Home from "./home/Home";
+import NotFound from "./NotFound";
 
 function Layout() {
   return (
-    <div>
-      <Header />
+    
       <div className="container">
+        {/* TODO: Implement the screen starting here */}
+        <Header />
         <Switch>
           <Route path="/">
-            <DeckHome />
+              <Home />
           </Route>
-          </Switch>
+          <Route>
+              <NotFound />
+          </Route>
+
+        </Switch>
+        
       </div>
-    </div>
   );
 }
 
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,8 +1,11 @@
+const { decks , cards }=require("../../data/db");
+
+
 /**
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
+//const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
@@ -10,210 +13,80 @@ const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
 
-/**
- * Removes the `cards` property from the deck so it is not accidentally saved with the deck.
- *
- * This function is NOT exported because it is not needed outside of this file.
- *
- * @param deck
- *  the deck instance
- * @returns {*}
- *  a copy of the deck instance with the `cards` property removed.
- */
-function stripCards(deck) {
-  const { cards, ...deckWithoutCards } = deck;
-  return deckWithoutCards;
-}
 
-/**
- * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
- *
- * This function is NOT exported because it is not needed outside of this file.
- *
- * @param url
- *  the url for the requst.
- * @param options
- *  any options for fetch
- * @param onCancel
- *  default value returned if the fetch is cancelled.
- * @returns {Promise<Error|any>}
- *  a promise that resolves to the `json` data or an error.
- *  If the response is not in the 200 - 399 range the promise is rejected.
- */
+// function stripCards(deck) {
+//   const { cards, ...deckWithoutCards } = deck;
+//   return deckWithoutCards;
+// }
 
-async function fetchJson(url, options, onCancel) {
-  try {
-    const response = await fetch(url, options);
 
-    if (response.status < 200 || response.status > 399) {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
 
-    if (response.status === 204) {
-      return null;
-    }
 
-    return await response.json();
-
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      throw error;
-    }
-    return Promise.resolve(onCancel);
-  }
-}
-
-/**
- * Retrieves all existing decks.
- * @returns {Promise<[deck]>}
- *  a promise that resolves to a possibly empty array of decks saved in the database.
- */
 export async function listDecks(signal) {
-  const url = `${API_BASE_URL}/decks?_embed=cards`;
-  return await fetchJson(url, { signal }, []);
+  const result=decks.map((deck)=>{
+    deck.cards=cards.filter(card=>card.deckId===deck.id);
+    return deck;
+  });
+  return result;
 }
 
-/**
- * Saves deck to the database (public/data/db.json).
- * There is no validation done on the deck object, any object will be saved.
- * @param deck
- *  the deck to save, which must not have an `id` property
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<deck>}
- *  a promise that resolves the saved deck, which will now have an `id` property.
- */
+let maxDeckId=decks.reduce((acc,{id})=>Math.max(acc,id),0);
+
 export async function createDeck(deck, signal) {
-  const url = `${API_BASE_URL}/decks`;
-  const options = {
-    method: "POST",
-    headers,
-    body: JSON.stringify(stripCards(deck)),
-    signal,
-  };
-  return await fetchJson(url, options, {});
+  deck.id=++maxDeckId;
+  decks.push(deck);
+  return deck;
 }
 
-/**
- * Retrieves the deck with the specified `deckId`
- * @param deckId
- *  the `id` property matching the desired deck.
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<any>}
- *  a promise that resolves to the saved deck.
- */
 export async function readDeck(deckId, signal) {
-  const url = `${API_BASE_URL}/decks/${deckId}?_embed=cards`;
-  return await fetchJson(url, { signal }, {});
+  const result= decks.find(deck=>deck.id===Number(deckId));
+  result.cards=cards.filter(card=>card.deckId===Number(deckId));
+  return result;
 }
 
-/**
- * Updates an existing deck
- * @param updatedDeck
- *  the deck to save, which must have an `id` property.
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<Error|*>}
- *  a promise that resolves to the updated deck.
- */
 export async function updateDeck(updatedDeck, signal) {
-  const url = `${API_BASE_URL}/decks/${updatedDeck.id}?_embed=cards`;
-  const options = {
-    method: "PUT",
-    headers,
-    body: JSON.stringify(stripCards(updatedDeck)),
-    signal,
-  };
-  return await fetchJson(url, options, updatedDeck);
+  updatedDeck.id=Number(updatedDeck.id);
+  const index=decks.findIndex(deck=>deck.id===updatedDeck.id);
+  decks[index]=updatedDeck;
+  return decks[index];
 }
 
-/**
- * Deletes the deck with the specified `deckId`.
- * @param deckId
- *  the id of the deck to delete
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<Error|*>}
- *  a promise that resolves to an empty object.
- */
 export async function deleteDeck(deckId, signal) {
-  const url = `${API_BASE_URL}/decks/${deckId}`;
-  const options = { method: "DELETE", signal };
-  return await fetchJson(url, options);
+  const index=decks.findIndex(deck=>deck.id===Number(deckId));
+  return decks.splice(index,1);
 }
 
-/**
- * Creates a new card associated with the specified `deckId`.
- * There is no validation that there is an existing deck with the specified `deckId`.
- * @param deckId
- *  the id of the target deck
- * @param card
- *  the card to create, which must not have an `id` property
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<Error|*>}
- *  a promise that resolves to the new card, which will have an `id` property.
- */
+
+export async function listCards(deckId, signal) {
+  const deckCards=cards.filter(card=>card.deckId===Number(deckId));
+  return deckCards;
+}
+
+let maxCardId=cards.reduce((acc,{id})=>Math.max(acc,id),0);
+
 export async function createCard(deckId, card, signal) {
   // There is a bug in json-server, if you post to /decks/:deckId/cards the associated deckId is a string
   // and the card is not related to the deck because the data types of the ID's are different.
-  const url = `${API_BASE_URL}/cards`;
-  card.deckId = Number(deckId);
-  const options = {
-    method: "POST",
-    headers,
-    body: JSON.stringify(card),
-    signal,
-  };
-  return await fetchJson(url, options, card);
+  
+  card.id=++maxCardId;
+  card.deckId=Number(deckId);
+  cards.push(card);
+  return card;
 }
 
-/**
- * Retrieves the card with the specified `cardId`
- * @param cardId
- *  the id of the target
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<Error|*>}
- *  a promise that resolves to the saved card.
- */
 export async function readCard(cardId, signal) {
-  const url = `${API_BASE_URL}/cards/${cardId}`;
-  return await fetchJson(url, { signal }, {});
+  const result= cards.find(card=>card.id===Number(cardId));
+  return result;
 }
 
-/**
- * Updates an existing deck
- * @param updatedCard
- *  the card to save, which must have an `id` property.
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<Error|*>}
- *  a promise that resolves to the updated card.
- */
 export async function updateCard(updatedCard, signal) {
-  const url = `${API_BASE_URL}/cards/${updatedCard.id}`;
-  const options = {
-    method: "PUT",
-    headers,
-    body: JSON.stringify(updatedCard),
-  };
-  return await fetchJson(url, options, updatedCard);
+  updatedCard.id=Number(updatedCard.id);
+  const index=cards.findIndex(card=>card.id===updatedCard.id);
+  cards[index]=updatedCard;
+  return cards[index];
 }
 
-/**
- * Deletes the card with the specified `cardId`.
- * @param cardId
- *  the id of the card to delete
- * @param signal
- *  optional AbortController.signal
- * @returns {Promise<Error|*>}
- *  a promise that resolves to an empty object.
- */
 export async function deleteCard(cardId, signal) {
-  const url = `${API_BASE_URL}/cards/${cardId}`;
-  const options = { method: "DELETE", signal };
-  return await fetchJson(url, options);
+  const index=cards.findIndex(card=>card.id===Number(cardId));
+  return cards.splice(index,1);
 }
