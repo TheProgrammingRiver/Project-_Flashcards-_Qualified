e274c898b653d03402dc54033a347ba9bd073a4f
TheProgrammingRiver <114197158+TheProgrammingRiver@users.noreply.github.com>
2022-12-14 18:29:25 -0500

    one

--- src/Layout/decks/study/StudyCards.js
+++ src/Layout/decks/study/StudyCards.js
@@ -1,52 +1,80 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
-// import { listCards } from "../../../utils/api";
 
-function StudyCards({cards}){
-    
-    const [flip,setFlip]=useState(false);
-    const [currentIndex,setCurrentIndex]=useState(0);
+function StudyCards({ cards }) {
+  const [flip, setFlip] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const history=useHistory();
+  const history = useHistory();
 
-    const handleNext=()=>{
-        if (currentIndex===cards.length-1){
-            const result = window.confirm("Restart cards? \n\nClick 'Cancel' to return to the home page.");
-            if (result){
-                setFlip(false);
-                setCurrentIndex(0);
-                return;
-                //history.go(0);
-            }else{
-                history.push("/");
-            }
-        }
-        setCurrentIndex((current)=>Math.min(cards.length-1,current+1));
-        setFlip(!flip);
-    };
-
-    
-    if (cards.length<=2){
-        return (
-            <div>
-                <h2>Not enough cards.</h2>
-                <p>You need at least 3 cards to study. There are {cards.length} in the deck.</p>
-            </div>
-        );
+  const handleNext = () => {
+    if (currentIndex === cards.length - 1) {
+      const result = window.confirm(
+        "Restart cards? \n\nClick 'Cancel' to return to the home page."
+      );
+      if (result) {
+        setFlip(false);
+        setCurrentIndex(0);
+        return;
+        //history.go(0);
+      } else {
+        history.push("/");
+      }
     }
-    
-    return(
-        <div className="card border border-primary mb-5">
-            <div className="card-body">
-                <h5 className="card-title">Card {currentIndex+1} of {cards.length}</h5>
-                <p className="card-text">{flip? cards[currentIndex].back : cards[currentIndex].front}</p>
-                <button type="button" className="btn btn-dark mr-2" onClick={()=>{setFlip(!flip)}}>Flip</button>
-                {flip && <button type="button" className="btn btn-primary" onClick={handleNext}>Next</button>}
-            </div>
-        </div>
-    );
+    setCurrentIndex((current) => Math.min(cards.length - 1, current + 1));
+    setFlip(!flip);
+  };
 
-    
+  if (cards.length <= 2) {
+    return (
+      <div>
+        <h2>Not enough cards.</h2>
+        <p>
+          You need at least 3 cards to study. There are {cards.length} in the
+          deck.
+        </p>
+        <button
+          onClick={() => history.push("/decks/new")}
+          type="button"
+          to="/"
+          className="btn btn-primary"
+        >
+          <span className="oi oi-plus "></span> Add Cards
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="card border border-primary mb-5">
+      <div className="card-body">
+        <h5 className="card-title">
+          Card {currentIndex + 1} of {cards.length}
+        </h5>
+        <p className="card-text">
+          {flip ? cards[currentIndex].back : cards[currentIndex].front}
+        </p>
+        <button
+          type="button"
+          className="btn btn-dark mr-2"
+          onClick={() => {
+            setFlip(!flip);
+          }}
+        >
+          Flip
+        </button>
+        {flip && (
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default StudyCards;
